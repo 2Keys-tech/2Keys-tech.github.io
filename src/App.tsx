1703797b0f17e3e87eb901a63b85269935588602
Madeline H <james.s.hart@outlook.com>
2020-05-11 21:16:39 +0100

    add styling and others

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import './App.css';
 
-export default class App extends React.Component {
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import type { RouteChildrenProps } from 'react-router-dom';
+
+import { FlexColumn, RootContainer } from './components/layout';
+import { Navbar, NavItem } from './components/Navbar';
+import { FeaturesRoot } from './routes/Features';
+import { OUTLINE_STANDARD } from './util/theme';
+
+class App extends React.Component<RouteChildrenProps, {}> {
 	render = () => {
 		return (
-			<section>
-				<h1>Hello!</h1>
-			</section>
-		)
+			<FlexColumn grow maxHeight>
+				<FlexColumn style={{ width: "100%", position: "relative" }} maxHeight>
+					<Navbar style={{ borderBottomWidth: 1, borderBottomStyle: "solid", borderBottomColor: OUTLINE_STANDARD }}>
+						<NavItem to="/Features/">Features</NavItem>
+						<NavItem to="/About/">Why</NavItem>
+						<NavItem to="/Company/">Company</NavItem>
+					</Navbar>
+					<Switch>
+						<Route path="/Features/" component={FeaturesRoot} />
+						<Route render={() => <Redirect to="/Features/" />} />
+					</Switch>
+				</FlexColumn>
+				{/* <RootContainer>
+				</RootContainer> */}
+			</FlexColumn>
+		);
 	}
-}
+};
+
+const AppContainer = () => (
+	<Router>
+		<Route component={App} />
+	</Router>
+);
+
+export default AppContainer;
